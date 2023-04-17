ab2de77a691999efaa367561b2efb3b3a9fd16fc
Didier Barradas Bautista <didier.barradas@gmail.com>
2023-04-17 10:51:41 +0300

    added new libs for dry-run

--- tests/test_two.py
+++ tests/test_two.py
@@ -4,3 +4,10 @@ import cuml
 import cupy
 import numpy as np
 import pandas as pd
+
+# print the versions
+print("cudf version:", cudf.__version__)
+print("cuml version:", cuml.__version__)
+print("cupy version:", cupy.__version__)
+print("numpy version:", np.__version__)
+print("pandas version:", pd.__version__)
